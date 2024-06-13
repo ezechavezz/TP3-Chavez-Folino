fcd9fd35285f285d9e02b0810947a11e43c4755f
ezechavezz <ezequielmartinchavez@gmail.com>
2024-06-13 13:13:42 -0300

    client

--- client.py
+++ client.py
@@ -3,12 +3,15 @@ import socket
 def iniciar_cliente():
     socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
-    direccion = ('127.0.0.1', 12345)
-    socket.connect(direccion)
+    direccion_servidor = ('127.0.0.1', 12345)
+    socket.connect(direccion_servidor)
     
-    print("Cliente conectado al servidor.")
+    print("Conectado.")
 
-    mensaje = "Hola, servidor!"
+    mensaje = "Hola"
     socket.sendall(mensaje.encode())
 
     socket.close()
+
+if __name__ == "__main__":
+    iniciar_cliente()
